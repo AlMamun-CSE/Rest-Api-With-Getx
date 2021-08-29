fb217ca8e1d5fb9425aafaf910ef622563271103
AlMamun-CSE <almamuncse2020@gmail.com>
2021-08-29 21:38:37 +0600

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:api_and_getx/Screens/HomeScreen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
